4cad62e003b8aa865ce1e0a627a326f73fb593e8
Devesh Duptala <dduptala@gmail.com>
2024-09-26 23:28:25 +1200

    ✨ Feature: Initially added the login and welcome screen

---
+++ P4P-Project/Views /LoginView.swift
@@ -0,0 +1,41 @@
+//
+//  LoginView.swift
+//  P4P-Project
+//
+//  Created by Devesh Duptala on 26/09/2024.
+//
+
+import SwiftUI
+
+// Login View
+struct LoginView: View {
+    @State private var name: String = "" // State to hold the entered name
+
+    var body: some View {
+        VStack {
+            Text("ECSE Asset Tracker")
+                .font(.largeTitle)
+                .padding()
+
+            TextField("Enter your name", text: $name)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .padding(.horizontal)
+
+            NavigationLink(destination: WelcomeView(name: name)) {
+                Text("Enter")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+            }
+            .disabled(name.isEmpty) // Disable the button if the name is empty
+        }
+        .padding()
+    }
+}
+
