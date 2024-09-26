c0d4f30c9436e187f53c6bb4f4c906fa266ed605
Devesh Duptala <dduptala@gmail.com>
2024-09-26 22:31:25 +1200

    Initial Commit

---
+++ P4P-Project/P4P_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  P4P_ProjectApp.swift
+//  P4P-Project
+//
+//  Created by Devesh Duptala on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct P4P_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ P4P-ProjectTests/P4P_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  P4P_ProjectTests.swift
+//  P4P-ProjectTests
+//
+//  Created by Devesh Duptala on 26/09/2024.
+//
+
+import XCTest
+@testable import P4P_Project
+
+final class P4P_ProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
